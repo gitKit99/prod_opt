e30e6e4c84bd70ad6c210925b08f97587164f248
Stepan Savchuk <s.savchuk@asd-team.com>
2021-05-25 10:28:57 +0300

    added some fields (min and max values) to db entitiesand assigned them appropriate values

--- prod_opt/main/models.py
+++ prod_opt/main/models.py
@@ -15,6 +15,14 @@ class Task(models.Model):
 
 class Component(models.Model):
     name = models.CharField('Name', max_length=50)
+    yMin = models.FloatField('ymin', default=0.0)
+    yMax = models.FloatField('ymax', default=100.0)
+    vMin = models.FloatField('vmin', default=0.0)
+    vMax = models.FloatField('vmax', default=100.0)
+    wMin = models.FloatField('wmin', default=0.0)
+    wMax = models.FloatField('wmax', default=100.0)
+    q0 = models.FloatField('q', default=0.0)
+    q1 = models.FloatField('^q', default=0.0)
 
     def __str__(self):
         return self.name
@@ -23,6 +31,8 @@ class Component(models.Model):
 class Bottle(models.Model):
     cost = models.FloatField('c')
     name = models.CharField('Name', max_length=50)
+    xMin = models.IntegerField('xmin', default=0)
+    xMax = models.IntegerField('xmax', default=0)
 
     def __str__(self):
         return self.name + " - " + str(self.cost)
